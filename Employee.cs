24391c12b70838b8688f4280911ec5d08ec08aa5
Ali <72344723+alisabourii@users.noreply.github.com>
2024-02-02 10:42:59 +0300

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace LLP.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Word { get; set; }
+        public string Explo {  get; set; }
+        public DateTime date { get; set; }
+
+        public int IdDeutsch { get; set; }
+        public string Wort { get; set; }
+        public string Erlauterung { get; set; }
+        public DateTime Datum { get; set; }
+
+    }
+}
