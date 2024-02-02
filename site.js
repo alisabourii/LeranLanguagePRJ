b7e5aebe554bf7db8ab6247add9b1c9e698c4e58
Ali <72344723+alisabourii@users.noreply.github.com>
2024-02-02 10:40:53 +0300

    Add files via upload

---
+++ site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function saveDataDeutsch() {
+    const wort = document.getElementById("wortID").value;
+    const ewrt = document.getElementById("erlaID").value;
+    alert(wort);
+    alert(ewrt);
+    
+}
+function saveDataEnglish() {
+    const word = document.getElementById("wordID").value;
+    const explo = document.getElementById("exploID").value;
+    alert(word);
+    alert(explo);
+}
