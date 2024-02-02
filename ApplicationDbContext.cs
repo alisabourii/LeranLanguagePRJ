5338d19eba0f02d983a2b886abb45d9ae4705f35
Ali <72344723+alisabourii@users.noreply.github.com>
2024-02-02 10:39:55 +0300

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using LLP.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+
+namespace LLP.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> contextOptions)
+            : base(contextOptions) 
+        { 
+        }
+        public DbSet<Employee> Employees { get; set; }
+        /*
+            use LLPDatabase;
+            INSERT INTO dbo.DeutschWort(wort,Erlauterung,Datum)
+            VALUES('Tag','Guten Tag','2024-01-30');
+
+            use LLPDatabase;
+            INSERT INTO dbo.EnglishWords(Word,Explanation,NowDate)
+            VALUES('Resume','This is my Resume of last 2 years','2024-01-30');
+        */
+
+    }
+
+}
